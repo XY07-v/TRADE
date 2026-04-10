ef8d6d67b5e9f0b4610ad1292789faba7949f395
XY07-v <edwar.vaco@gmail.com>
2026-04-10 07:33:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ CONFIG_FORMULARIOS = {
         "titulo": "Food Service", "icono": "fa-utensils",
         "preguntas": [
             {"id": "negocio", "label": "Nombre del Establecimiento", "type": "text", "required": True},
-            {"id": "producto_1", "label": "Productos_1", "type": "multiselect", "options": 
+            {"id": "producto_1", "label": "Productos_1", "type": "select", "options": 
              ["Aro - Caldo Desmenuzado 800 Grs",
                 "Aro - Caldo gallina 1 Kg",
                 "Aro - Base bechamel Aro x 1 Kg",
@@ -78,7 +78,7 @@ CONFIG_FORMULARIOS = {
                 "Carreta - Leche condensada la Carreta x 5 Kg"], "required": True},
             
             {"id": "Precio_1", "label": "Precio_1",  "type": "text", "required": True},
-            {"id": "producto_2", "label": "Productos_2", "type": "multiselect", "options": 
+            {"id": "producto_2", "label": "Productos_2", "type": "select", "options": 
              ["Aro - Caldo Desmenuzado 800 Grs",
                 "Aro - Caldo gallina 1 Kg",
                 "Aro - Base bechamel Aro x 1 Kg",
@@ -91,6 +91,19 @@ CONFIG_FORMULARIOS = {
                 "Carreta - Leche condensada la Carreta x 5 Kg"], "required": True},
             
             {"id": "Precio_2", "label": "Precio_2",  "type": "text", "required": True},
+            {"id": "producto_3", "label": "Productos_3", "type": "select", "options": 
+             ["Aro - Caldo Desmenuzado 800 Grs",
+                "Aro - Caldo gallina 1 Kg",
+                "Aro - Base bechamel Aro x 1 Kg",
+                "Aro - Leche condesada Aro x 1.3 Kg",
+                "Aro - Leche condesada Aro x 3.9 Kg",
+                "Aro - Leche condesada Aro x 5 Kg",
+                "Bugueña - Leche condensada la bugueña x 1 Kg",
+                "Bugueña - Leche condensada la bugueña x 5 Kg",
+                "Carreta - Leche condensada la Carreta x 1 Kg",
+                "Carreta - Leche condensada la Carreta x 5 Kg"], "required": True},
+            
+            {"id": "Precio_3", "label": "Precio_3",  "type": "text", "required": True},
         ]
     },
     "competencias": {
